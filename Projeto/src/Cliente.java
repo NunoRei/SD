2ad4b57200e90aa25b212ee2618f92e46eb69e37
NunoRei <36609841+NunoRei@users.noreply.github.com>
2018-12-18 01:21:07 +0000

    Start

---
+++ Projeto/src/Cliente.java
@@ -0,0 +1,26 @@
+/**
+ *
+ * @author João Marques, Nuno Rei e Jaime Leite
+ */
+public class Cliente {
+    private String user_name;
+    private String password;
+    private float value_to_pay;
+    
+    //Criação de um Cliente
+    public Cliente (String nome, String pass) {
+        this.user_name = nome;
+        this.password = pass;
+        this.value_to_pay = 0;
+    }
+    
+    //Verifica palavra passe
+    public boolean Verifica_Pass(String pass) {
+        return this.password.equals(pass);
+    }
+    
+    //Retornar valor a dever
+    public float getValue_to_Price() {
+        return this.value_to_pay;
+    }
+}
---
+++ Projeto/src/Servidor.java
@@ -0,0 +1,70 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+/**
+ *
+ * @author João Marques, Nuno Rei e Jaime Leite
+ */
+public class Servidor {
+    private String serverName;
+    private String serverType;
+    private static ServerSocket SS;
+    private String clienteAtual;
+    private float pricePerHour;
+    
+    //Construtor de um Servidor
+    public Servidor(String nome, String tipo, float price) {
+        this.serverName = nome;
+        this.serverType = tipo;
+        this.pricePerHour = price;
+    }
+    
+    //Colocar o cliente no servidor
+    public void addCliente(String nome) {
+        this.clienteAtual = nome;
+    }
+    
+    //Retirar o cliente do servidor
+    public void removeCliente() {
+        this.clienteAtual = null;
+    }
+    
+    //Retornar nome do cliente
+    public String getClienteAtual() {
+        return this.clienteAtual;
+    }
+    
+    //Atualizar preço por hora em caso de leilão
+    public void setPricePerHour(float price) {
+        this.pricePerHour = price;
+    }
+    
+    //Enquanto o cliente estiver a escrever para o servidor
+    //Falta colocar Times para calcular o tempo que o cliente esteve no servidor para adicionar há sua dívida
+    public float init(int port) throws IOException {
+        SS = new ServerSocket(port);
+        Socket x;
+        //Time timeInicio = Time.now();
+        while (true) {
+            x = SS.accept();
+            PrintWriter out = new PrintWriter(x.getOutputStream());
+            BufferedReader in = new BufferedReader(new InputStreamReader(x.getInputStream()));
+            while (true) {
+                String s = in.readLine();
+                if(s.equals("exit")) break;
+                out.println(s);
+                out.flush();
+            }
+            out.close();
+            x.close();
+            removeCliente();
+            break;
+        }
+        //Time timeFim = Time.now();
+        float price = 0;//(toHours(timeFim) - toHours(timeInicio))*pricePerHour;
+        return price;
+    }
+}
