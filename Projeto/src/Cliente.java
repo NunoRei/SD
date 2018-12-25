91951454fa0b34b17cb526218b726bc710b53ad3
jaimeleite <32790077+jaimeleite@users.noreply.github.com>
2018-12-25 20:56:48 +0000

    Update Cliente.java

--- Projeto/src/Cliente.java
+++ Projeto/src/Cliente.java
@@ -1,4 +1,3 @@
-//import Servidor.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -19,7 +18,7 @@ public class Cliente{
         //if(true == ValidaCliente(args[0],args[1])) {
 
             ClienteStub c = new ClienteStub();
-
+            int aut = 0;
             /*
             System.out.println("Agora está conetado ao servidor principal");
             System.out.println("1 -> servers para reservar");
@@ -37,7 +36,7 @@ public class Cliente{
                         if (reg == 0) System.out.println("Registado");
                         break;
                     case "autentica":
-                        int aut = c.autenticaCliente(p[1], p[2]);
+                        aut = c.autenticaCliente(p[1], p[2]);
                         if (aut == 0) {
                             System.out.println("Autenticado com sucesso");
                             f = false;
@@ -45,7 +44,7 @@ public class Cliente{
                         break;
                         
                     case "servidor_Pedido":
-                        int aut = c.reservarPorPedido(p[1], p[2]);
+                        aut = c.reservarPorPedido(p[1], p[2]);
                         if (aut == 0) {
                             System.out.println("O seu pedido foi registado com sucesso");
                             f = false;
@@ -53,7 +52,8 @@ public class Cliente{
                         break;
 
                     case "servidor_Leilao":
-                        int aut = c.reservarPorLeilao(p[1], p[2], p[3]);
+                    	double value = Double.parseDouble(p[2]);
+                        aut = c.reservarPorLeilao(p[1], value, p[3]);
                         if (aut == 0) {
                             System.out.println("O seu pedido foi introduzido no leilão com sucesso");
                             f = false;
@@ -65,20 +65,18 @@ public class Cliente{
                     */
                     case "exit":
                             //p[1] é o nickname do cliente
-                            int pretendeSairExit = st.retiraServidorExit(p[1]);
+                            int pretendeSairExit = c.retiraServidorExit(p[1]);
                             if(pretendeSairExit == 0)
                                 System.out.println("Logout efetuado com sucesso");
                         break;
-
-                    //caso em que o cliente faz contol+C
-                    case null:
-                            int pretendeSairNull = st.retiraServidorNull();
-                             if(pretendeSairNull == 0)
-                                System.out.println("Logout efetuado com sucesso");
-                        break;
-
+                    //se if entao é porque n passou nada para o servidor senao é pq passou um comando inválido
                     default:
-                        System.out.println("Comando invalido.");
+                    	if(s == null){ 
+	                    	int pretendeSairNull = c.retiraServidorNull();
+	                        if(pretendeSairNull == 0)
+	                            System.out.println("Logout efetuado com sucesso");
+                    	}
+                    	else System.out.println("Comando invalido.");
                 }
             }
                 //out.println(s);
