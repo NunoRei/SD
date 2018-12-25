aab9f8e4d0b573771e326bc97c79dc367cf32e07
jaimeleite <32790077+jaimeleite@users.noreply.github.com>
2018-12-25 20:55:35 +0000

    Update Servidor.java

--- Projeto/src/Servidor.java
+++ Projeto/src/Servidor.java
@@ -49,44 +49,43 @@ public class Servidor implements Runnable{
                     case "autentica":
                         int resautentica = st.autenticaCliente(p[1], p[2]);
                         s = Integer.toString(resautentica);
-                        if(s == 0)
+                        if(resautentica == 0)
                             this.clientesConectados.put(p[0],x);
                         break;
                         
                     case "servidor_Pedido":
                         //caso esteja conectado
-                        if(this.clientesConectados.contains(p[1])){
-                            String resservPedido = st.reservarPorPedido(p[1], p[2]);
-                            s = resservPedido;
+                        if(this.clientesConectados.containsKey(p[1])){
+                            int resservPedido = st.reservarPorPedido(p[1], p[2]);
+                            s = Integer.toString(resservPedido);
                         }
                         break;
 
                     //para aqui tem de indicar o nickname de utilizador, para ver se ja esta autenticado, o preço horário e indicar o tipo de servidor que quer reservar
                     case "servidor_Leilao":
-                        if(this.clientesConectados.contains(p[1])){
-                            int resservLeilao = st.reservarPorLeilao(p[1], p[2], p[3]);
-                            s = resservLeilao;
+                        if(this.clientesConectados.containsKey(p[1])){
+                        	double value = Double.parseDouble(p[2]);
+                            int resservLeilao = st.reservarPorLeilao(p[1], value, p[3]);
+                            s = Integer.toString(resservLeilao);
                         }
                         break;
                     
                     //caso em que o cliente escreve exit para sair so sistema
                     case "exit":
-                        if(this.clientesConectados.contains(p[1])){
-                            String pretendeSairExit = st.retiraServidor(p[1]);
-                            s = pretendeSair;
-                        }
-                        break;
-
-                    //caso em que o cliente faz contol+C
-                    case null:
-                            //variavel nickname reulta de ir ao map de clientes conectados e ficar com a key do socket em questao
-                            String resservLeilao = st.retiraServidor(nickname);
-                            s = resservLeilao;
+                        if(this.clientesConectados.containsKey(p[1])){
+                            int pretendeSairExit = st.retiraServidorExit(p[1]);
+                            s = Integer.toString(pretendeSairExit);
                         }
                         break;
 
                     default:
-                        System.out.println("Comando invalida.");
+                    	//se if entao é porque n passou nada para o servidor senao é pq passou um comando inválido
+                    	if(s == null){
+                    		//variavel nickname reulta de ir ao map de clientes conectados e ficar com a key do socket em questao
+                            //String resservLeilao = st.retiraServidor(nickname);
+                            //s = resservLeilao;
+                    	}
+                    	else System.out.println("Comando invalido.");
                 }
                 //if (s.equals("3")) break;
                 out.println(s);
@@ -94,6 +93,7 @@ public class Servidor implements Runnable{
                 //a parte de remover o cliente tem de ser aqui, senao perde-se o socket e nao se consegue fazer o println e o flush
                 if(p[0].equals("exit") || (s == null)) this.clientesConectados.remove(p[1]);
             }
+        }
             //out.close();
             //x.close();
         catch (Exception e){
