bd4f681fcf1a8cad9070fdd1873f4437073b3cfb
jaimeleite <32790077+jaimeleite@users.noreply.github.com>
2018-12-29 20:22:33 +0000

    definition of server changed

--- Projeto/src/Catalogo.java
+++ Projeto/src/Catalogo.java
@@ -1,30 +1,30 @@
-import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
-
-/**
- *
- * @author João Marques, Nuno Rei, Jaime Leite e Hugo Nogueira
- */
+import java.util.concurrent.locks.Condition;
 
 public class Catalogo{
-    private static class servidor{
-        private String id;
+	private static class Servidor {
         private String type;
         private Double preco;
-        private int estado;
+        private int quantidade;
+        private ReentrantLock lock;
+        private Condition isFree;
+        
 
         //estado 1 a ser usado e 0 a nao ser usado
-        private servidor(String id, String type, Double preco, int estado){
-            this.id = id;
+        public Servidor(String type, Double preco, int quantidade){
+            //this.id = id;
             this.type = type;
             this.preco = preco;
-            this.estado = estado;
+            this.quantidade = quantidade;
+            isFree = lock.newCondition();
         }
 
-        public String getId(){
+        /*public String getId(){
             return this.id;
-        }
+        }*/
 
         public String getType(){
             return this.type;
@@ -34,68 +34,68 @@ public class Catalogo{
             return this.preco;
         }
 
-        public int getEstado(){
-            return this.estado;
+        public int getQuantidade(){
+            return this.quantidade;
         }
+	}
 
-        public void setEstado(int i){
-            this.estado = i;
-        }
-    }
+	/**
+	 *
+	 * @author João Marques, Nuno Rei, Jaime Leite e Hugo Nogueira
+	 */
+	    private Map<String,Servidor> servidores = new HashMap<>();
+	    //private ArrayList<servidor> pedido = new ArrayList<>();
+	    private ReentrantLock lockservers = new ReentrantLock();
 
-    private ArrayList<servidor> servidores = new ArrayList<>();
-    //private ArrayList<servidor> pedido = new ArrayList<>();
-    private ReentrantLock lockservers = new ReentrantLock();
+	    //0 se esta para ser reservado a pedido; 1 se esta a ser usado; 2 se esta para ser reservado em leilao
+	    public Catalogo(){
+	    	servidores.put("micro",new Servidor("micro",0.99,3));
+	        servidores.put("medium",new Servidor("medium",1.50,4));
+	        servidores.put("large",new Servidor("large",1.00,5));
+	    }
 
-    //0 se esta para ser reservado a pedido; 1 se esta a ser usado; 2 se esta para ser reservado em leilao
-    public Catalogo(){
-        servidores.add(new servidor("t3.micro","micro",0.99,0));
-        servidores.add(new servidor("t3.large","large",1.50,0));
-        servidores.add(new servidor("m5.micro","micro",0.50,2));
-        servidores.add(new servidor("m5.large","large",1.00,2));
-    }
+	    //retorna uma posicao do array que esteja livre
+	    //indica se existe um servidor daquele tipo disponivel para reserva a pedido
+	    /*public int existeServerPedido(String type){
+	        int i;
+	        for(i=0; i<this.servidores.size(); i++){
+	            if(this.servidores.get(i).getType().equals(type) && this.servidores.get(i).getEstado() == 0) return i;
+	        }
+	        return -1;
+	    }
 
-    //retorna uma posicao do array que esteja livre
-    //indica se existe um servidor daquele tipo disponivel para reserva a pedido
-    public int existeServerPedido(String type){
-        int i;
-        for(i=0; i<this.servidores.size(); i++){
-            if(this.servidores.get(i).getType().equals(type) && this.servidores.get(i).getEstado() == 0) return i;
-        }
-        return -1;
-    }
+	    //retorna uma posicao do array que esteja livre
+	    //indica se existe um servidor daquele tipo disponivel para reserva a leilao
+	    public int existeServerLeilao(String type){
+	        int i;
+	        for(i=0; i<this.servidores.size(); i++){
+	            if(this.servidores.get(i).getType().equals(type) && this.servidores.get(i).getEstado() == 2) return i;
+	        }
+	        return -1;
+	    }
+	    */
 
-    //retorna uma posicao do array que esteja livre
-    //indica se existe um servidor daquele tipo disponivel para reserva a leilao
-    public int existeServerLeilao(String type){
-        int i;
-        for(i=0; i<this.servidores.size(); i++){
-            if(this.servidores.get(i).getType().equals(type) && this.servidores.get(i).getEstado() == 2) return i;
-        }
-        return -1;
-    }
+	    public Map<String,Servidor> getCatalogo(){
+	        return this.servidores;
+	    }
 
-    public ArrayList<servidor> getCatalogo(){
-        return this.servidores;
-    }
+	    /*muda a disponibilidade de um servidor no array de servidores: 1 se passa a estar ocupado; 0 ou 2 se volta a estar livre 
+	        para ser reservado por pedido ou por leilao, respetivamente
+	     
+	    public void setOcupied(int posicao, int novoEstado){
+	        this.servidores.get(posicao).setEstado(novoEstado);
+	    }
 
-    /*muda a disponibilidade de um servidor no array de servidores: 1 se passa a estar ocupado; 0 ou 2 se volta a estar livre 
-        para ser reservado por pedido ou por leilao, respetivamente
-    */ 
-    public void setOcupied(int posicao, int novoEstado){
-        this.servidores.get(posicao).setEstado(novoEstado);
-    }
+	    //metodo para indicar que um servidor passa a estar livre
+	    public void setFree(int posicao, int novoEstado){
+	        this.servidores.get(posicao).setEstado(novoEstado);  
+	    }*/
 
-    //metodo para indicar que um servidor passa a estar livre
-    public void setFree(int posicao, int novoEstado){
-        this.servidores.get(posicao).setEstado(novoEstado);  
-    }
+	    public void lock(){
+	        this.lockservers.lock();
+	    }
 
-    public void lock(){
-        this.lockservers.lock();
-    }
-
-    public void unlock(){
-        this.lockservers.unlock();
-    }
+	    public void unlock(){
+	        this.lockservers.unlock();
+	    }
 }
